c555bc23b8c4defe098fecebc597827a68f3d5c5
emcraciu <ozy24us@gmail.com>
2025-03-03 21:05:45 +0200

    modul 1

---
+++ modul1/part1.py
@@ -0,0 +1,67 @@
+# # Integer
+# a = 10
+# print('Result of 10 + 12 = ', (10).__add__(12))
+# print(f"Integer: {a}")
+#
+# # Float
+# b = 10.7
+# print(f"Float: {b}")
+#
+# # Complex
+# c = 3 + 6j
+# print(f"Complex: {c}")
+#
+# # addition
+# print(f"{type(a + b)}")
+#
+#
+#
+#
+#
+# # String manipulation
+# name = "Alice"
+# name = 'Alice'
+# name = """Alice
+# """
+# name = '''Alice
+# '''
+# name = f"Alice\n {a}"
+# name = r"Alice\n"
+# name = u"Alice\nabc"
+#         #012345 678
+# print(name[0::2])  # Accessing the first character
+# print(name)
+# # # String methods
+# print(name.upper())  # Uppercase
+# print(name.lower())  # Lowercase
+# print(name.replace("Al", "Bob"))  # Replace Alice with Bob
+
+
+# # List (mutable)
+# fruits = ["apple", 12, "cherry"]
+# print(id(fruits))
+# fruits.append("orange")  # Add an item
+# print(fruits)
+# print(id(fruits))
+#
+# name = "Alice"
+# print(id(name))
+# name = name.upper()
+# print(id(name))
+#
+# number1 = 1055555 + 200
+# print(id(number1))
+# number2 = 1055655 + 100
+# print(id(number2))
+#
+# # Tuple (immutable)
+# coordinates = (10, 20)
+# print(coordinates)
+
+
+# Boolean values
+is_raining = True
+is_sunny = False
+print(is_raining and is_sunny)  # False
+print(is_raining or is_sunny)  # True
+
