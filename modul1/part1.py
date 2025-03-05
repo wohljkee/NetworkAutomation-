1221658cfde5278225a3fe02059910431d6c9b32
emcraciu <ozy24us@gmail.com>
2025-03-05 19:30:59 +0200

    modul 1l2

--- modul1/part1.py
+++ modul1/part1.py
@@ -59,9 +59,75 @@
 # print(coordinates)
 
 
-# Boolean values
-is_raining = True
-is_sunny = False
-print(is_raining and is_sunny)  # False
-print(is_raining or is_sunny)  # True
+# # Boolean values
+# is_raining = True
+# is_sunny = False
+# print(is_raining and is_sunny)  # False
+# print(is_raining or is_sunny)  # True
 
+# # Sets
+# set1 = {1, 2, 2, 3}
+# print(set1)
+# set1.add(4)
+# print(set1)
+# set1.add(1)
+# print(set1)
+# print((5).__hash__())
+# print(('1').__hash__())
+# # print(('1').__hash__())
+#
+# print([1,2])
+# list1 = [1]
+# print(id(list1))
+# list1.append(2)
+# print(id(list1))
+# print(list1.__hash__)
+
+# print(set1.difference({2,3,5}))
+# print({2,3,5}.difference(set1))
+# print(set1.intersection({2,3,5}))
+
+# # dict
+# dict1 = {'key1': '1', 'key2': 2, 'key3': []} #, {1:1}: 3} - non hashable objects cannot be keys
+# print(dict1)
+# print(dict1.get('key1'))
+# print(dict1['key2'])
+# print(dict1.get('key4', "not present"))
+# # print(dict1['key4']) # will generate exception when key not present
+# dict1.update({'new_KEY': set()}) # update method does not return anything (returns None)
+# print(dict1)
+#
+# print('keys resutns', dict1.keys(), 'with type', type(dict1.keys()))
+# print(list(dict1.keys())[1])
+#
+# print('keys resutns', dict1.values(), 'with type', type(dict1.values()))
+# print(list(dict1.values())[1])
+
+
+# x = 8
+# if x > 10:
+#     print("x is greater than 10")
+# elif x == 10:
+#     print("x equals 10")
+# elif x == 8:
+#     print("x equals 8")
+# else:
+#     print("x is less than 10")
+
+# numbers = [1, 2, 3, 4, 5]
+# for number in numbers:
+#     print(number, end='\t')
+
+# numbers = [1, 2, 3, 4, 5]
+# for number in numbers:
+#     if number == 3:
+#         numbers.remove(number)
+#     print(number, end='\t')
+# print(numbers)
+
+numbers = [1, 2, 3, 4, 5]
+for number in numbers.copy():
+    if number == 3:
+        numbers.remove(number)
+    print(number, end='\t')
+print(numbers)
