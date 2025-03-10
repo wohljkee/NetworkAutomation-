c1f79cb48787c7c834088360e484ee5ebf64afa7
emcraciu <ozy24us@gmail.com>
2025-03-10 19:36:59 +0200

    modul2

--- modul2/ex1.py
+++ modul2/ex1.py
@@ -6,7 +6,7 @@ def show_menu():
     print("5. Snack                 ...5lei")
     print("6. Meniu sandwich +cola  ...11lei")
 
-
+valid_mony = [1,5,10,50,100]
 while True:
     show_menu()
     choice = input("Enter your choice: ")
@@ -19,3 +19,12 @@ while True:
     except ValueError:
         print("Invalid choice, please try again")
         continue
+
+    money = input("Give money: ")
+    try:
+        money = int(money)
+    except ValueError:
+        print("\nIncorrect money, please try again")
+        continue
+    if money in valid_mony:
+        pass
