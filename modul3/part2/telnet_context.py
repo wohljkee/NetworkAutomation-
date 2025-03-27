bfd6f496e9258bd6ce950775114ee1dd60904d8f
emcraciu <ozy24us@gmail.com>
2025-03-27 20:05:40 +0200

    modul3

--- modul3/part2/telnet_context.py
+++ modul3/part2/telnet_context.py
@@ -1,23 +1,27 @@
 import telnetlib
-import time
 
 
 class TelnetContext:
-    def __init__(self, host, port):
-        self.host = host
+    def __init__(self, address: str, port: int, hostname: bytes):
+        self.address = address
         self.port = port
         self.connection = None
+        self.hostname = hostname
 
     def __enter__(self):
-        self.connection = telnetlib.Telnet(self.host, self.port)
+        self.connection = telnetlib.Telnet(self.address, self.port)
         self.connection.write(b"\n")
-        time.sleep(3)
         out = self.connection.read_very_eager()
-        if b'(config)' in out:
-            self.connection.write(b'exit')
-        elif b'config-' in out:
-            self.connection.write(b'end')
+        # print(out.decode())
+        if b'(config)' in out or b'(config-' in out:
+            self.connection.write(b"end\n")
         return self
+
+    def write(self, command: bytes):
+        self.connection.write(command + b"\n")
+
+    def expect(self, regex: list[bytes]):
+        self.connection.expect([self.hostname + regex[0]])
 
     def __exit__(self, type, value, traceback):
         self.connection.close()
