1e685e5832914b28b7e6abecc1d4a53a3f459ccf
emcraciu <ozy24us@gmail.com>
2025-04-02 18:12:11 +0300

    modul4

--- modul4/part1/async_module.py
+++ modul4/part1/async_module.py
@@ -7,19 +7,23 @@ import random
 
 async def sleep():
     print('function before')
-    await asyncio.sleep(random.randint(1,10))
+    await asyncio.sleep(random.randint(1,3))
+    time.sleep(5)
     async with aiohttp.ClientSession() as session:
-        response = await session.request('GET', 'http://worldtimeapi.org/api/timezone/America')
+        response = await session.request('GET', 'https://worldtimeapi.org/api/timezone')
     print(await response.text())
 
 
 async def main():
-    task = await asyncio.gather(*(sleep() for _ in range(10)))
+    task = await asyncio.gather(*(sleep() for _ in range(3)))
 
 
 if __name__ == '__main__':
     star_time = time.time()
     print(star_time)
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except:
+        pass
     end_time = time.time()
     print(end_time - star_time)
