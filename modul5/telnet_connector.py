a1550d510cde3fbff175909c1d0a331c0af8e805
emcraciu <ozy24us@gmail.com>
2025-04-11 19:50:07 +0300

    modul4

--- modul5/telnet_connector.py
+++ modul5/telnet_connector.py
@@ -1,7 +1,15 @@
+import telnetlib
+
+
 class TelnetConnector:
 
     def __init__(self):
-        pass
+        self.conn = None
 
     def connect(self, *args, **kwargs):
-        pass
+        address = kwargs['address']
+        port = kwargs['port']
+        self.conn = telnetlib.Telnet(host=address, port=port)
+
+    def do_initial_configuration(self):
+        self.conn.execute("")
