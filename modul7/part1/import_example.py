6e484db1f7e54fb103a87cc3c19b4bd4568356a0
emcraciu <ozy24us@gmail.com>
2025-04-23 18:41:17 +0300

    modul5 homework

---
+++ modul7/part1/import_example.py
@@ -0,0 +1,6 @@
+from modul6.part1.telnet_connector import TelnetConnector
+# from ... import modul6
+
+# TelnetConnector
+from pyats.topology import loader
+tb = loader.load('modul6/part1/testbed_example.yaml')
