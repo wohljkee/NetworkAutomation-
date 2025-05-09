358703619a5cbb897b529a726c812f04c5be8eb4
emcraciu <ozy24us@gmail.com>
2025-05-09 19:30:08 +0300

    modul9

--- module9/part1/part9.py
+++ module9/part1/part9.py
@@ -1,5 +1,29 @@
 import pylint
+import os
+from multiprocessing import Process
 
-args = ['--disable=missing-class-docstring,missing-function-docstring,too-few-public-methods',
-        'modul7/part2/configre_ospf.py']
-pylint.run_pylint(args)
+# args = ['--disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,too-few-public-methods', 'modul7/part2/configre_ospf.py']
+# pylint.run_pylint(args)
+
+dir = os.getcwd()
+print(dir)
+scan_list = []
+
+# option 1 - running on directory with same-structure
+# pylint.run_pylint([dir])
+
+# option2 - run for each file in folder structure
+for root, _, files in os.walk(dir):
+    for file in files:
+        result = os.path.join(root, file)
+        if result.endswith('.py'):
+            scan_list.append(result)
+
+if __name__ == '__main__':
+    process_list = []
+    for file in scan_list:
+        p = Process(target=pylint.run_pylint, args=([file],))
+        p.start()
+        process_list.append(p)
+    for p in process_list:
+        p.join()
