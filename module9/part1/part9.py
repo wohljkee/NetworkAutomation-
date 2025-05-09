cce53b6967c14ac2daf4ce4d18f161c9fdd9de53
emcraciu <ozy24us@gmail.com>
2025-05-09 19:38:25 +0300

    modul9

--- module9/part1/part9.py
+++ module9/part1/part9.py
@@ -13,17 +13,22 @@ scan_list = []
 # pylint.run_pylint([dir])
 
 # option2 - run for each file in folder structure
-for root, _, files in os.walk(dir):
-    for file in files:
-        result = os.path.join(root, file)
-        if result.endswith('.py'):
-            scan_list.append(result)
+# for root, _, files in os.walk(dir):
+#     for file in files:
+#         result = os.path.join(root, file)
+#         if result.endswith('.py'):
+#             scan_list.append(result)
+#
+# if __name__ == '__main__':
+#     process_list = []
+#     for file in scan_list:
+#         p = Process(target=pylint.run_pylint, args=([file],))
+#         p.start()
+#         process_list.append(p)
+#     for p in process_list:
+#         p.join()
 
-if __name__ == '__main__':
-    process_list = []
-    for file in scan_list:
-        p = Process(target=pylint.run_pylint, args=([file],))
-        p.start()
-        process_list.append(p)
-    for p in process_list:
-        p.join()
+# option 3 - run with pylint_rc file
+# pylint will check dir for RC file or use provided path
+args = ['--rcfile=pylintrc', 'modul7/part2/configre_ospf.py']
+pylint.run_pylint(args)
