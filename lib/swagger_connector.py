dea7b767f9a1eddd6d274c205d45145f651ac2a0
emcraciu <ozy24us@gmail.com>
2025-05-17 12:49:51 +0300

    modul9

--- lib/swagger_connector.py
+++ lib/swagger_connector.py
@@ -1,10 +1,13 @@
+import json
 from typing import Optional
 
 import requests
 import urllib3
+from bravado.client import SwaggerClient
 from bravado.requests_client import RequestsClient
 from pyats.datastructures import AttrDict
 from pyats.topology import Device
+
 
 
 class SwaggerConnector:
@@ -12,28 +15,48 @@ class SwaggerConnector:
     def __init__(self, device: Device, **kwargs):
         self._session = None
         self._auth = None
-        self._headers = None
+        self._headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
         self._url = None
         self._url_login = None
         self.device = device
         self.connection: Optional[AttrDict] = None
         self.api_endpoints: list[str] = None
+        self.client: Optional[SwaggerClient] = None
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     def connect(self, **kwargs):
         self.connection = kwargs['connection']
+        endpoint = '/apispec/ngfw.json'
         self._url = f'https://{self.connection.ip.compressed}:{self.connection.port}'
         self.__login(
             self.connection.credentials.login.username,
             self.connection.credentials.login.password.plaintext
         )
+        self._headers.update({'Authorization': f'{self.token_type} {self.access_token}'})
         https_client = RequestsClient()
         https_client.session.verify = False
+        https_client.ssl_verify = False
         https_client.session.headers = self._headers
+        swagger_client = SwaggerClient.from_url(
+            self._url + endpoint,
+            http_client=https_client,
+            request_headers=self._headers,
+            config={'validate_certificate': False, 'validate_responses': False},
+        )
+        self.client = swagger_client
 
     def __login(self, username: Optional[str] = None, password: Optional[str] = None):
-        endspoint = '/api/fdm/v3/fdm/token'
-        requests.post(self._url + endspoint)
+        endspoint = '/api/fdm/latest/fdm/token'
+        response = requests.post(
+            self._url + endspoint,
+            verify=False,
+            data=json.dumps({'username': username, 'password': password, 'grant_type': 'password'}),
+            headers=self._headers
+        )
+        self.access_token = response.json()['access_token']
+        self.token_type = response.json()['token_type']
+        self.refresh_token = response.json()['refresh_token']
+
 
     def disconnect(self):
         pass
--- modul8/part1/configure_fdm_via_rest.py
+++ modul8/part1/configure_fdm_via_rest.py
@@ -4,6 +4,9 @@ from pyats import aetest
 from pyats.aetest.steps import Steps
 from pyats.topology import loader
 
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 from lib.swagger_connector import SwaggerConnector
 
 tb = loader.load('testbed_example.yaml')
@@ -16,6 +19,16 @@ class Example3(aetest.Testcase):
         with steps.start('Connect to FDM'):
             swagger: SwaggerConnector = device_fdm.connections.rest['class'](device_fdm)
             swagger.connect(connection=device_fdm.connections.rest)
+        with steps.start('create security zone'):
+            security_zone = swagger.client.get_model('SecurityZone')
+
+
+        with steps.start("configure Interface"):
+            interface = swagger.client.get_model('PhysicalInterface')
+
+
+        with steps.start('Create network object'):
+            network_object = swagger.client.get_model('NetworkObject')
 
 
 if __name__ == '__main__':
